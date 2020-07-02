ac838e24e855bf6180b3d51e820c6fdd0106618f
Phillip Nguyen <c-phillip.nguyen@hulu.com>
2020-07-01 17:45:10 -0700

    add e2e brs test

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -1,12 +1,14 @@
 import { RoAssociativeArray, BrsString, mGlobal } from "../brsTypes";
 import { mockComponent } from "./mockComponent";
 import { mockFunction } from "./mockFunction";
+import { mockComponentPartial } from "./mockComponentPartial";
 import { RunInScope } from "./RunInScope";
 import { Process } from "./Process";
 
 export const _brs_ = new RoAssociativeArray([
     { name: new BrsString("mockComponent"), value: mockComponent },
     { name: new BrsString("mockFunction"), value: mockFunction },
+    { name: new BrsString("mockComponentPartial"), value: mockComponentPartial },
     { name: new BrsString("runInScope"), value: RunInScope },
     { name: new BrsString("process"), value: Process },
     { name: new BrsString("global"), value: mGlobal },
